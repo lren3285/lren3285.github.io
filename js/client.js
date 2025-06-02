54a82dd7a9471aba947d35dcbb9098f145a9c9a1
jsaussy <135028340+jsaussy@users.noreply.github.com>
2025-06-02 16:46:06 -0400

    Update client.js

--- js/client.js
+++ js/client.js
@@ -7,6 +7,15 @@ TrelloPowerUp.initialize({
 	'card-buttons': function(t, options) {
 		return [{
 			text: 'Open confirm',
+			        callback: function (t) {
+          			  return t.popup({
+            			    title: "Confirm",
+            			    type: 'confirm',
+				    message: 'Confirm?',
+				    confirmText: 'Confirm!',
+				    onConfirm: () => {console.log('confirm');},
+         			   });
+				},
 		}];
 	},
 }, {targetOrigin: "https://trellis.coffee"});
