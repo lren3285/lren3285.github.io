c8a7390336881a2817e3f6273a3c52639c2659fe
jsaussy <135028340+jsaussy@users.noreply.github.com>
2025-06-03 09:38:59 -0400

    Update client.js

--- js/client.js
+++ js/client.js
@@ -18,4 +18,18 @@ TrelloPowerUp.initialize({
 				},
 		}];
 	},
+		'board-buttons': function(t, options) {
+		return [{
+			text: 'Open confirm',
+			        callback: function (t) {
+          			  return t.popup({
+            			    title: "Confirm",
+            			    type: 'confirm',
+				    message: 'Confirm?',
+				    confirmText: 'Confirm!',
+				    onConfirm: () => {console.log('confirm');},
+         			   });
+				},
+		}];
+	},
 }, {targetOrigin: "http://localhost:2999/"});
