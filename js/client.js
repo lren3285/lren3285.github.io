975078b40c1c5299044e95fd1cd620bb3a2f9ce6
jsaussy <135028340+jsaussy@users.noreply.github.com>
2025-06-03 10:41:55 -0400

    Update client.js

--- js/client.js
+++ js/client.js
@@ -30,6 +30,34 @@ TrelloPowerUp.initialize({
 				    onConfirm: () => {console.log('confirm');},
          			   });
 				},
-		}];
+		},
+			{
+				text: 'Open list',
+				callback: function (t) {
+					return t.popup({
+						title: 'List!'
+						items: [
+						{
+							text: 'Item 1',
+							callback: function (t, opts) {
+								console.log('Item 1');
+							}
+						},
+												{
+							text: 'Item 2',
+							callback: function (t, opts) {
+								console.log('Item 2');
+							}
+						},
+												{
+							text: 'Item 3',
+							callback: function (t, opts) {
+								console.log('Item 3');
+							}
+						}
+						],
+					});
+				},
+			}];
 	},
 }, {targetOrigin: "https://trellis.coffee"});
