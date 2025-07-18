fc1774c7e8489152f250792bd9a0e786dfe71bd9
lren3285 <lren@atlassian.com>
2025-07-18 11:23:04 -0400

    Update client.js

--- js/client.js
+++ js/client.js
@@ -34,6 +34,47 @@ TrelloPowerUp.initialize(
             });
           },
         },
+            {
+          text: "Open modal",
+          callback: function(t) {
+            t.modal({
+  // the url to load for the iframe
+  url: "js/iframe.html",
+  // optional arguments to be passed to the iframe as query parameters
+  // access later with t.arg('text')
+  args: { text: 'Hello' },
+  // optional color for header chrome
+  accentColor: '#F2D600',
+  // initial height for iframe
+  height: 500, // not used if fullscreen is true
+  // whether the modal should stretch to take up the whole screen
+  fullscreen: true,
+  // optional function to be called if user closes modal (via `X` or escape, etc)
+  callback: () => console.log('Goodbye.'),
+  // optional title for header chrome
+  title: 'appear.in meeting',
+  // optional action buttons for header chrome
+  // max 3, up to 1 on right side
+  actions: [{
+    icon: './images/icon.svg',
+    callback: function (t) { return t.closeModal()},
+    alt: 'Leftmost',
+    position: 'left',
+  }, {
+    icon: './images/icon.svg',
+    callback: function (tr) {return tr.popup({
+      title: tr.localizeKey('appear_in_settings'),
+      url: 'js/iframe.html',
+      height: 164,
+    })},
+    alt: 'Second from left',
+    position: 'left',
+  }, {
+    icon: './images/icon.svg',
+    callback: function (t) { return t.updateModal({accentColor: '#FFFFFF', actions: [], fullScreen: true, title: "updated"})},
+    alt: 'Right side',
+    position: 'right',
+  }
       ];
     },
     "board-buttons": function (t, options) {
