4b9f79cfcfee2a46a379e8a2f237090db00bffe5
lren3285 <lren@atlassian.com>
2025-12-02 15:05:36 -0500

    Update client.js

--- js/client.js
+++ js/client.js
@@ -1,104 +1,112 @@
 /* global TrelloPowerUp */
 
-var Promise = TrelloPowerUp.Promise;
+const Promise = TrelloPowerUp.Promise;
 
 TrelloPowerUp.initialize(
   {
-    "card-buttons": function (t, options) {
+    'card-buttons'(t, options) {
       return [
         {
-          text: "Open confirm",
-          callback: function (t) {
+          text: 'Open confirm',
+          callback(t) {
             return t.popup({
-              title: "Confirm",
-              type: "confirm",
-              message: "Confirm?",
-              confirmText: "Confirm!",
+              title: 'Confirm',
+              type: 'confirm',
+              message: 'Confirm?',
+              confirmText: 'Confirm!',
               onConfirm: () => {
-                console.log("confirm");
+                return t.popup({
+                  title: 'Next confirm',
+                  type: 'confirm',
+                  message: 'Confirm?',
+                  confirmText: 'Confirm!',
+                  onConfirm: () => {
+                    console.log('confirm');
+                  },
+                });
               },
             });
           },
         },
         {
-          text: "Open date",
-          callback: function (t) {
+          text: 'Open date',
+          callback(t) {
             t.popup({
-              type: "date",
-              title: "DaTE!!!!",
-              callback: function (t, opts) {
+              type: 'date',
+              title: 'DaTE!!!!',
+              callback(t, opts) {
                 console.log(opts.date);
               },
-              minDate: new Date("2025-03-01"),
+              minDate: new Date('2025-03-01'),
               maxDate: new Date(),
             });
           },
         },
         {
-          text: "Open iframe",
-          callback: function (t) {
+          text: 'Open iframe',
+          callback(t) {
             return t.popup({
-              title: "iframe",
-              type: "iframe",
-              url: "js/iframe.html",
+              title: 'iframe',
+              type: 'iframe',
+              url: 'js/iframe.html',
               height: 200,
             });
           },
         },
         {
-          text: "Open modal",
-          callback: function (t) {
+          text: 'Open modal',
+          callback(t) {
             t.modal({
               // the url to load for the iframe
-              url: "js/iframe.html",
+              url: 'js/iframe.html',
               // optional arguments to be passed to the iframe as query parameters
               // access later with t.arg('text')
-              args: { text: "Hello" },
+              args: { text: 'Hello' },
               // optional color for header chrome
-              accentColor: "#F2D600",
+              accentColor: '#F2D600',
               // initial height for iframe
               height: 500, // not used if fullscreen is true
               // whether the modal should stretch to take up the whole screen
               fullscreen: true,
               // optional function to be called if user closes modal (via `X` or escape, etc)
-              callback: () => console.log("Goodbye."),
+              callback: () => console.log('Goodbye.'),
               // optional title for header chrome
-              title: "appear.in meeting",
+              title: 'appear.in meeting',
               // optional action buttons for header chrome
               // max 3, up to 1 on right side
               actions: [
                 {
-                  icon: "./images/icon.svg",
-                  callback: function (t) {
-                    return t.sizeTo(300);
+                  icon: './images/icon.svg',
+                  callback(t) {
+                    return t.closeModal();
                   },
-                  alt: "Leftmost",
-                  position: "left",
+                  alt: 'Leftmost',
+                  position: 'left',
                 },
                 {
-                  icon: "./images/icon.svg",
-                  callback: function (tr) {
+                  icon: './images/icon.svg',
+                  callback(tr) {
                     return tr.popup({
-                      title: tr.localizeKey("appear_in_settings"),
-                      url: "js/iframe.html",
+                      title: tr.localizeKey('appear_in_settings'),
+                      url: 'js/iframe.html',
                       height: 164,
                     });
                   },
-                  alt: "Second from left",
-                  position: "left",
+                  alt: 'Second from left',
+                  position: 'left',
                 },
                 {
-                  icon: "./images/icon.svg",
-                  callback: function (t) {
+                  icon: './images/icon.svg',
+                  callback(t) {
                     return t.updateModal({
-                      accentColor: "#FFFFFF",
+                      accentColor: '#FFFFFF',
                       actions: [],
-                      fullScreen: true,
-                      title: "updated",
+                      fullscreen: true,
+                      title: 'updated',
                     });
                   },
-                  alt: "Right side",
-                  position: "right",
+                  alt: 'Right side',
+                  position: 'right',
                 },
               ],
             });
@@ -106,96 +114,100 @@ TrelloPowerUp.initialize(
         },
       ];
     },
-    "board-buttons": function (t, options) {
+    'board-buttons'(t, options) {
       return [
         {
-          text: "Open confirm",
-          callback: function (t) {
+          text: 'Open confirm',
+          callback(t) {
             return t.popup({
-              title: "Confirm",
-              type: "confirm",
-              message: "Confirm?",
-              confirmText: "Confirm!",
+              title: 'Confirm',
+              type: 'confirm',
+              message: 'Confirm?',
+              confirmText: 'Confirm!',
               onConfirm: () => {
-                console.log("confirm");
+                console.log('confirm');
               },
             });
           },
         },
         {
-          text: "Open list",
-          callback: function (t) {
+          text: 'Open list',
+          callback(t) {
             return t.popup({
-              title: "List!",
-              items: function (t, options) {
-      // use options.search which is the search text entered so far
-      // return a Promise that resolves to an array of items
-      // similar to the items you provided in the client side version above
-      return new Promise(function (resolve) {
-        // you'd probably be making a network request at this point
-        resolve([{
-          text: 'Result 1',
-          callback: function (t, opts) { ... }
-        }, {
-          text: 'Result 2',
-          callback: function (t, opts) { ... }
-        }]);
-      });
-    },
+              title: 'List!',
+              items(args) {
+                // use args.options.search which is the search text entered so far
+                // return a Promise that resolves to an array of items
+                // similar to the items you provided in the client side version above
+                return new Promise(function (resolve) {
+                  // you'd probably be making a network request at this point
+                  resolve([
+                    {
+                      text: 'Result 1',
+                      callback(t, opts) {
+                        console.log(opts);
+                      },
+                    },
+                    {
+                      text: 'Result 2',
+                      callback(t, opts) {
+                        console.log(opts);
+                      },
+                    },
+                  ]);
+                });
+              },
               search: {
                 count: 5,
-                placeholder: "placeholder",
-                empty: "no search results",
-                searching: "searching....",
+                placeholder: 'placeholder',
+                empty: 'no search results',
+                searching: 'searching....',
               },
             });
           },
         },
         {
-          text: "Open board bar",
-          callback: function (t) {
+          text: 'Open board bar',
+          callback(t) {
             return t.boardBar({
               // required URL to load in the iframe
-              url: "js/iframe.html",
+              url: 'js/iframe.html',
               // optional arguments to be passed to the iframe as query parameters
               // access later with t.arg('text')
-              args: { text: "Hello" },
+              args: { text: 'Hello' },
               // optional color for header chrome
-              accentColor: "#F2D600",
+              accentColor: '#F2D600',
               // initial height for iframe
               height: 200, // initial height for iframe
               // optional function to be called if user closes modal
-              callback: () => console.log("Goodbye."),
+              callback: () => console.log('Goodbye.'),
               // optional boolean for whether the user should
               // be allowed to resize the bar vertically
               resizable: true,
               // optional title for header chrome
-              title: "Board Meeting",
+              title: 'Board Meeting',
               // optional action buttons for header chrome
               // max 3, up to 1 on right side
               actions: [
                 {
-                  icon: "./images/icon.svg",
-                  url: "https://google.com",
-                  alt: "Leftmost",
-                  position: "left",
+                  icon: './images/icon.svg',
+                  url: 'https://google.com',
+                  alt: 'Leftmost',
+                  position: 'left',
                 },
                 {
-                  icon: "./images/icon.svg",
-                  callback: (tr) =>
-                    tr.popup({
-                      title: tr.localizeKey("appear_in_settings"),
-                      url: "settings.html",
-                      height: 164,
-                    }),
-                  alt: "Second from left",
-                  position: "left",
+                  icon: './images/icon.svg',
+                  callback(t) {
+                    return t.closeBoardBar();
+                  },
+                  alt: 'Second from left',
+                  position: 'left',
                 },
                 {
-                  icon: "./images/icon.svg",
-                  callback: () => console.log(":tada:"),
-                  alt: "Right side",
-                  position: "right",
+                  icon: './images/icon.svg',
+                  callback: () => console.log(':tada:'),
+                  alt: 'Right side',
+                  position: 'right',
                 },
               ],
             });
@@ -204,5 +216,5 @@ TrelloPowerUp.initialize(
       ];
     },
   },
-  { targetOrigin: "https://trellis.coffee" }
+  { targetOrigin: 'http://localhost:2999/' },
 );
